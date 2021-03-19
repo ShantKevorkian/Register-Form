536cb51e0cf319156c5347de80373fb505887e29
Shant Kevorkian <shantkevv@gmail.com>
2021-03-19 20:51:08 +0300

    Modified and Added

--- internship.sql
+++ internship.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 19, 2021 at 08:59 AM
+-- Generation Time: Mar 19, 2021 at 05:49 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -33,15 +33,18 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `user_id` int(11) NOT NULL,
   `comment` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=92 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`id`, `user_id`, `comment`, `created_at`) VALUES
-(45, 11, 'sad', '2021-03-19 08:58:04');
+INSERT INTO `comments` (`id`, `user_id`, `comment`, `created_at`, `updated_at`) VALUES
+(91, 15, 'dog', '2021-03-19 17:39:38', ''),
+(90, 15, 'car', '2021-03-19 17:39:24', ''),
+(89, 16, 'test\r\n', '2021-03-19 17:39:11', '');
 
 -- --------------------------------------------------------
 
@@ -56,7 +59,17 @@ CREATE TABLE IF NOT EXISTS `user_reg` (
   `email` varchar(100) NOT NULL,
   `password` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_reg`
+--
+
+INSERT INTO `user_reg` (`id`, `name`, `email`, `password`) VALUES
+(17, 'raffi', 'raffi95@outlook.com', '25f9e794323b453885f5181f1b624d0b'),
+(16, 'Shant Kevorkian', 'shant97@outlook.com', '25f9e794323b453885f5181f1b624d0b'),
+(15, 'nazo', 'nazo@gmail.com', '25f9e794323b453885f5181f1b624d0b'),
+(14, 'Shant Kevorkian', 'shant1997@outlook.com', '25f9e794323b453885f5181f1b624d0b');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
