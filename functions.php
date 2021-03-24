ea868e80029d9924e88befbe275d946ca106153c
Shant Kevorkian <shantkevv@gmail.com>
2021-03-24 10:43:22 +0300

    Modified and Added

--- functions.php
+++ functions.php
@@ -23,8 +23,7 @@
         curl_setopt($curlInitialize, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?q=Yerevan&appid=9638c0cc9efbbfc00e4493a1effc4199&units=metric");
         curl_setopt($curlInitialize, CURLOPT_RETURNTRANSFER, true);
 
-        $sql = "SELECT city_name, temperature ,weatherTime FROM weather_data
-                WHERE city_name = 'Yerevan'";
+        $sql = "SELECT city_name, temperature ,weatherTime FROM weather_data";
         $runQuery = $conn->query($sql);
 
         if($runQuery->num_rows == 0) {
@@ -34,7 +33,7 @@
             $city =  $weather_data->name;
             $temp = $weather_data->main->temp;
             $query = "INSERT INTO weather_data (city_name, temperature, weatherTime)
-                            VALUES ('$city', $temp, CURRENT_TIME())";
+                            VALUES ('$city', $temp, '$currentDate')";
             
             if(!$conn->query($query)) {
                 echo "Database error";
@@ -43,16 +42,21 @@
         }
 
         else {
-            $result = curl_exec($curlInitialize);
-            curl_close($curlInitialize);
-            $weather_data = json_decode($result);
-            $updatedTemp = $weather_data->main->temp;
-            $queryUpdate = "UPDATE weather_data SET weatherTime = CURRENT_TIME(), temperature = $updatedTemp
-                            WHERE (CURRENT_TIME() - weatherTime > 1800)";
-            
-            if(!$conn->query($queryUpdate)) {
-                echo "Database error";
-                exit;
+            $row = $runQuery->fetch_assoc();
+            $time = $row['weatherTime'];
+            $dbTime = strtotime($time);
+            if (time() - $dbTime > 1800) {
+                $result = curl_exec($curlInitialize);
+                curl_close($curlInitialize);
+                $weather_data = json_decode($result);
+                $updatedTemp = $weather_data->main->temp;
+                $queryUpdate = "UPDATE weather_data SET weatherTime = CURRENT_TIME(), temperature = $updatedTemp
+                                WHERE (CURRENT_TIME() - weatherTime > 1800)";
+                
+                if(!$conn->query($queryUpdate)) {
+                    echo "Database error";
+                    exit;
+                }
             }
             
         }
